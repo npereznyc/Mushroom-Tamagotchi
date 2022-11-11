1a49646c8b305a4ae3cd4c5013ff03abece63faf
npereznyc <natalieperez84@gmail.com>
2022-11-11 13:21:25 -0500

    Updated feed, lights, and play buttons to decrement the hungry, sleepy, and bored metrics by 1 when clicked.

---
+++ README.txt
@@ -0,0 +1,12 @@
+HTML is set up.
+JS is started:
+- [x]  Input form for name
+- [x]  Submit button for name
+- [x]  Display name
+- [x]  Start button
+- [x]  Start timer
+- [x]  Start metrics
+- [ ]  Set metrics to stop at 10
+- [ ]  When any of metrics hit 10, end game
+- [x]  Create feed/sleep/play buttons
+- [x]  Set buttons to decrement the metrics when clicked
--- main.js
+++ main.js
@@ -1,28 +1,58 @@
-//START: Timer & Metrics
+//SUBMIT NAME
+
+//targets:
+const form=document.querySelector('#form') //encompasses entire form, up to start button
+const input=document.querySelector('#name') //input field
+
+
+//handler
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); //prevents the page from automatically reloading every time the submit button is clicked
+    const inputName=input.value; //saving the user's input as inputName
+
+    console.log(inputName)
+
+    event.target.reset()
+
+    const displayName=document.createElement('h4')
+    displayName.innerHTML=inputName
+    document.querySelector('#nameInput').appendChild(displayName)
+
+
+})
+
 
 //targets:
 let timer=document.querySelector('#timerCount');
-const startTimer=document.querySelector('#startTimer');
 let startingAge=document.querySelector('#age');
 let startingHunger=document.querySelector('#hunger');
 let startingSleep=document.querySelector('#sleep');
 let startingBored=document.querySelector('#bored');
 
-//handler
-startTimer.addEventListener('click', function() {
+//handlers
+startBtn.addEventListener('click', function() {
     let time=Number(timer.textContent);
     let age=Number(startingAge.textContent);
     let hunger=Number(startingHunger.textContent);
     let sleep=Number(startingSleep.textContent);
     let bored=Number(startingBored.textContent);
 
+    //Number() takes the number 0 from the html (a string) and converts it to a number that JS can work with.
+
+    let feedBtn=document.querySelector('#feedBtn');
+    let lightsBtn=document.querySelector('#lightsBtn');
+    let playBtn=document.querySelector('#playBtn');
+
+    
+
     function updateTimer(num) {
-    timer.textContent=num;
+    timer.textContent=num; //changing the content of 'timer' to 'num', which is a placeholder in the updateTimer function(for now), for a value that will be passed into it...
 }
     interval=setInterval(()=> {
         time+=1;
         updateTimer(time);
-    }, 1000)
+    }, 1000) 
+    //This interval takes the value of 'time' and increments it by 1, it then passes the new value of 'time' to the updateTimer function, updating the content of 'timer' to the incremented value. It does this every second.
 
     function updateAge(num) {
         startingAge.textContent=num;
@@ -40,6 +70,11 @@ startTimer.addEventListener('click', function() {
         updateHunger(hunger);
     }, 4000)
 
+    feedBtn.addEventListener('click', function () {
+        hunger-=1;
+        updateHunger(hunger)
+    })
+
     function updateSleep(num) {
         startingSleep.textContent=num;
     }
@@ -47,6 +82,11 @@ startTimer.addEventListener('click', function() {
         sleep+=1;
         updateSleep(sleep);
     }, 6000)
+
+    lightsBtn.addEventListener('click', function () {
+        sleep-=1;
+        updateSleep(sleep)
+    })
 
     function updateBored(num) {
         startingBored.textContent=num;
@@ -57,32 +97,10 @@ startTimer.addEventListener('click', function() {
         updateBored(bored);
     }, 3000)
 
+    playBtn.addEventListener('click', function () {
+        bored-=1;
+        updateBored(bored)
+    })
+
     startTimer.disabled=true;
-})
-
-//SUBMIT NAME
-
-//targets:
-const form=document.querySelector('#form')
-const input=document.querySelector('#name')
-
-//dom update
-// function updateName(input) {
-//     displayName.textContent=input;
-// }
-
-//handler
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const input=document.querySelector('#name')
-    const inputName=input.value;
-    console.log(inputName)
-
-    event.target.reset()
-
-    const displayName=document.createElement('h4')
-    displayName.innerHTML=inputName
-    document.querySelector('#nameInput').appendChild(displayName)
-
-//updateName();
 })
